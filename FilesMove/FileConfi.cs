0fb9bcd623863776629050f137d380bdef5a7fed
AlanLang <langwdalan@gmail.com>
2018-01-10 10:35:40 +0800

    新建项目

---
+++ FilesMove/FileConfi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesMove
+{
+    public class FileConfi
+    {
+        public string version;
+        public string person;
+        public string basepath;
+        public List<files> files;
+    }
+
+    public class files
+    {
+        public string name;
+        public string path;
+    }
+}
